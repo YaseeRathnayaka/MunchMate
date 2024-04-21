090c2f89fb86f8b6456cb905859ac029b62b5cdd
Yasee Rathnayaka <rmybrathnayaka@std.appsc.sab.ac.lk>
2024-04-21 10:50:53 +0530

    button overlap make

--- frontend/src/components/pages/HomePage/HomePage.js
+++ frontend/src/components/pages/HomePage/HomePage.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Header from '../../containers/Header/Header'
 import slide3 from '../../../assets/slide3.jpg'
+import './HomePage.css'
 
 const HomePage = () => {
   return (
     <div className='home-container'>
-      <Header /> 
-        <div>
+      <div>
+        <Header />
+      </div>
+        <div className='image-container'>
             <img src={slide3} className='slide-3' />
+        <div className='overlap-button'>
+            <button className='order-button'>Order Now</button>
         </div>
+      </div>
     </div>
+
   )
 }
 
